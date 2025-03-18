18c4f15c14f7a3b0dd6be81424f35a2ecfaea7ff
Funkidalick24 <120457946+Funkidalick24@users.noreply.github.com>
2025-03-18 14:38:25 +0200

    added swagger ui documentation

--- controller/contacts.js
+++ controller/contacts.js
@@ -1,37 +1,30 @@
-const mongodb = require("../data/database");
-const ObjectId = require("mongodb").ObjectId;
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
 
 const getContacts = async (req, res) => {
-  try {
-    const result = await mongodb
-      .getDB()
-      .collection("contacts")
-      .find()
-      .toArray();
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    try {
+        const result = await mongodb.getDB().collection('contacts').find().toArray();
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 const getContact = async (req, res) => {
-  try {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDB()
-      .collection("contacts")
-      .findOne({ _id: userId });
-
-    if (!result) {
-      return res.status(404).json({ message: "Contact not found" });
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb.getDB().collection('contacts').findOne({ _id: userId });
+        
+        if (!result) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 module.exports = { getContacts, getContact };
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongodb = require("./data/database");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", require("./routes"));
+
 mongodb.initDB((err) => {
   if (err) {
     console.log(err);
